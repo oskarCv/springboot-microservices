ad0bef88f3fe85eb72b41b4bbd01bd5dc7d70ef0
System Administrator <root@darkozCv.local>
2020-04-14 17:20:07 -0500

    Initial commit --adding the initial spring applications template

---
+++ rating-book/src/main/java/com/oskarcv/ratingbook/RatingBookApplication.java
@@ -0,0 +1,13 @@
+package com.oskarcv.ratingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingBookApplication.class, args);
+	}
+
+}
---
+++ rating-book/src/test/java/com/oskarcv/ratingbook/RatingBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskarcv.ratingbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
