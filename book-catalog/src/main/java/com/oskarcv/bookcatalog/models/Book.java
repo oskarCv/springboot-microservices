36d9c48fe207bdb26ab4b170c0d339eaafe6183e
System Administrator <root@darkozCv.local>
2020-04-17 22:01:08 -0500

    adding comunication capavility with other service

---
+++ book-catalog/src/main/java/com/oskarcv/bookcatalog/models/Book.java
@@ -0,0 +1,30 @@
+package com.oskarcv.bookcatalog.models;
+
+public class Book {
+    private String bookId;
+    private String title;
+
+    public Book(){
+
+    }
+    public Book(String bookId, String title) {
+        this.bookId = bookId;
+        this.title = title;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- book-catalog/src/main/java/com/oskarcv/bookcatalog/resources/BookCatalogResource.java
+++ book-catalog/src/main/java/com/oskarcv/bookcatalog/resources/BookCatalogResource.java
@@ -1,19 +1,38 @@
 package com.oskarcv.bookcatalog.resources;
 
+import com.oskarcv.bookcatalog.models.Book;
 import com.oskarcv.bookcatalog.models.CatalogItem;
+import com.oskarcv.bookcatalog.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class BookCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(
-                new CatalogItem("Java8 in Action","Java 8 new features walk through",5));
+        //TODO: get all related books
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("123",5),
+                new Rating("456",4)
+        );
+        return ratings.stream()
+                .map(rating ->{
+                    Book book = restTemplate.getForObject("http://localhost:8082/books/"+rating.getBookId(), Book.class);
+                    return new CatalogItem(book.getTitle(), "desc",rating.getRating());
+                })
+                .collect(Collectors.toList());
+        //TODO: for each bookid call the boock info and get details
+        //TODO: put all the info together
     }
 }
